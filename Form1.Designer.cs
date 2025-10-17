270c899825fb114397b5f2aaa1e2461b29a6f40c
Moses Ma <701000372@GoTVDSB.ca>
2025-10-17 08:33:08 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace T9_A2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.lblAnswer1 = new System.Windows.Forms.Label();
+            this.lblAnswer2 = new System.Windows.Forms.Label();
+            this.lblAnswer3 = new System.Windows.Forms.Label();
+            this.lblAnswer4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btnAdd.Location = new System.Drawing.Point(21, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(68, 64);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btnSubtract.Location = new System.Drawing.Point(21, 82);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(68, 64);
+            this.btnSubtract.TabIndex = 1;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btnMultiply.Location = new System.Drawing.Point(21, 152);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(68, 64);
+            this.btnMultiply.TabIndex = 2;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btnDivide.Location = new System.Drawing.Point(21, 222);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(68, 64);
+            this.btnDivide.TabIndex = 3;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // lblAnswer1
+            // 
+            this.lblAnswer1.AutoSize = true;
+            this.lblAnswer1.Location = new System.Drawing.Point(109, 47);
+            this.lblAnswer1.Name = "lblAnswer1";
+            this.lblAnswer1.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer1.TabIndex = 4;
+            // 
+            // lblAnswer2
+            // 
+            this.lblAnswer2.AutoSize = true;
+            this.lblAnswer2.Location = new System.Drawing.Point(109, 117);
+            this.lblAnswer2.Name = "lblAnswer2";
+            this.lblAnswer2.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer2.TabIndex = 5;
+            // 
+            // lblAnswer3
+            // 
+            this.lblAnswer3.AutoSize = true;
+            this.lblAnswer3.Location = new System.Drawing.Point(109, 187);
+            this.lblAnswer3.Name = "lblAnswer3";
+            this.lblAnswer3.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer3.TabIndex = 6;
+            // 
+            // lblAnswer4
+            // 
+            this.lblAnswer4.AutoSize = true;
+            this.lblAnswer4.Location = new System.Drawing.Point(109, 244);
+            this.lblAnswer4.Name = "lblAnswer4";
+            this.lblAnswer4.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer4);
+            this.Controls.Add(this.lblAnswer3);
+            this.Controls.Add(this.lblAnswer2);
+            this.Controls.Add(this.lblAnswer1);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Label lblAnswer1;
+        private System.Windows.Forms.Label lblAnswer2;
+        private System.Windows.Forms.Label lblAnswer3;
+        private System.Windows.Forms.Label lblAnswer4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T9_A2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lblAnswer1.Text = "29 + 38 = " + (29 + 38);
+            Reset();
+            lblAnswer1.Visible = true;
+            btnAdd.Enabled = false;
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            lblAnswer2.Text = "420 - 353 = " + (420 - 353);
+            Reset();
+
+            lblAnswer2.Visible = true;
+            btnSubtract.Enabled= false;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            lblAnswer3.Text = "3 * 23 = " + (3 * 23);
+            Reset();
+
+            lblAnswer3.Visible = true;
+            btnMultiply.Enabled = false;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            lblAnswer4.Text = "138 / 2 = " + (138 / 2);
+            Reset();
+
+            lblAnswer4.Visible = true;
+            btnDivide.Enabled = false;
+        }
+
+        private void Reset()
+        {
+            lblAnswer1.Visible = false;
+            lblAnswer2.Visible = false;
+            lblAnswer3.Visible = false;
+            lblAnswer4.Visible = false;
+            btnAdd.Enabled = true;
+            btnSubtract.Enabled = true;
+            btnDivide.Enabled = true;
+            btnMultiply.Enabled = true;
+        }
+
+    }
+}
